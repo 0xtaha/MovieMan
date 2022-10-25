69f374be8546c0a5b493903eb642af5e2271d23c
Taha Abdalzez <57445405+0xtaha@users.noreply.github.com>
2022-10-25 16:21:06 +0200

    add header and footer basic info, routing, Movies Services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { FooterComponent } from './shared/footer/footer.component';
+import { HomeComponent } from './pages/home/home.component';
+import { MoviesComponent } from './pages/movies/movies.component';
+import { TvshowsComponent } from './pages/tvshows/tvshows.component';
+import { GenresComponent } from './pages/genres/genres.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent,
-    FooterComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [AppComponent, HeaderComponent, FooterComponent, HomeComponent, MoviesComponent, TvshowsComponent, GenresComponent],
+	imports: [BrowserModule, AppRoutingModule],
+	providers: [],
+	bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
